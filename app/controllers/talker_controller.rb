909d707e2b8591098e18f51b1f99f2a09857b594
hanbyul_kim <rlagksquf30@gmail.com>
2015-10-15 12:18:18 +0000

    basics

--- app/controllers/talker_controller.rb
+++ app/controllers/talker_controller.rb
@@ -1,4 +1,17 @@
 class TalkerController < ApplicationController
   def index
+    @chats = Chat.all
   end
+  
+  def create
+    talk = Talk.where(ask: params[:ask]).sample
+    
+    if talk == nil
+      redirect_to '/learn'
+    else
+    Chat.create(chat_type: "human", chat_content: talk.ask)
+    Chat.create(chat_type: "bot", chat_content: talk.answer)
+    redirect_to :root
+    end
+end
 end
